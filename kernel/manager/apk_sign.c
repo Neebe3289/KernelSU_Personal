8b0544743b2ae4533f2ccc8db432631f95223dd1
Neebe3289 <neebexd@gmail.com>
2026-07-31 12:29:57 +0700

    kernel: apk_sign: append own signature hash for manager
    Change-Id: If7074115793424963ed5add15072837bd1eef27b

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -362,15 +362,19 @@ bool is_manager_apk(char *path)
 	}
 #endif
 
-	// dummy.keystore
+	// This drivers
+	if (check_v2_signature(path, 0x388,"75df2885869bc947e1a524b2c793986a02bbb1427be21630e7b3ab226581b088"))
+		return true;
+
+	// XXKSU
 	if (check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549"))
 		return true;
 
-	 // kernelsu official
+	 // KernelSU Official
 	if (check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH))
 		return true;
 
-	// KOWX712/KernelSU
+	// KowSU
 	if (check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"))
 		return true;
 
