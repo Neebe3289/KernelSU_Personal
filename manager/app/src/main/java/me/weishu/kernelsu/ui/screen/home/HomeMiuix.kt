e9628171d22d4ce799c66d2632126bf1db42a595
Neebe3289 <neebexd@gmail.com>
2026-08-21 08:11:23 +0700

    personal
    This patch is using on top of backslashxx repos with a few change e.g. own JKS keystore, pkg name and partially from KOWX712.
    
    Co-authored-by: KOWX712 <leecc0503@gmail.com>
    Change-Id: I7794b4b316213eb64d251ddfb9c3c054c3d35a74

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMiuix.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMiuix.kt
@@ -116,9 +116,9 @@ fun HomePagerMiuix(
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.spacedBy(12.dp),
                     ) {
-                        if (state.checkUpdateEnabled) {
-                            UpdateCard(state = state, actions = actions)
-                        }
+//                        if (state.checkUpdateEnabled) {
+//                            UpdateCard(state = state, actions = actions)
+//                        }
                         if (state.showManagerPrBuildWarning) {
                             WarningCard(stringResource(id = R.string.home_pr_build_warning), level = WarningLevel.Notice)
                         } else if (state.showKernelPrBuildWarning) {
